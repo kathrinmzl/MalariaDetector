09a6e6ce1ab58db81486eae887edb23e18069a99
kathrinmzl <kathrin@muenzel.de>
2025-10-28 18:00:33 +0100

    update load_test_evaluation with Recall because I added it in the workbook

--- app_pages/page_ml_performance.py
+++ app_pages/page_ml_performance.py
@@ -26,6 +26,6 @@ def page_ml_performance_metrics():
     st.write("---")
 
     st.write("### Generalised Performance on Test Set")
-    st.dataframe(pd.DataFrame(load_test_evaluation(version), index=['Loss', 'Accuracy']))
+    st.dataframe(pd.DataFrame(load_test_evaluation(version), index=['Loss', 'Accuracy', 'Recall']))
     
     
